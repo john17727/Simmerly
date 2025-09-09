a4105e7cf710f8ed6c58957d9db71ac67979a786
Juan Rincon <justanotherone17727@gmail.com>
2025-09-08 22:32:48 -0400

    Refactor: Clean up SplashScreen.kt imports
    This commit removes unused imports from `SplashScreen.kt`:
    - `androidx.compose.foundation.layout.fillMaxWidth`
    - `androidx.compose.material3.Text`
    - `dev.juanrincon.simmerly.welcome.presentation.Logo`

--- composeApp/src/commonMain/kotlin/dev/juanrincon/simmerly/splash/presentation/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/juanrincon/simmerly/splash/presentation/SplashScreen.kt
@@ -5,9 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.sizeIn
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,7 +14,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import dev.juanrincon.simmerly.welcome.presentation.Logo
 import org.jetbrains.compose.resources.painterResource
 import simmerly.composeapp.generated.resources.Res
 import simmerly.composeapp.generated.resources.simmerly_logo
