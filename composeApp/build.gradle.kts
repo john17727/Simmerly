cb9992cdc55e156a49df546c27cc8ff7d6c852f2
Juan Rincon <justanotherone17727@gmail.com>
2026-01-31 11:47:19 -0500

    Update library versions.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -35,8 +35,8 @@ kotlin {
 
     sourceSets {
         androidMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.preview)
+            implementation(libs.runtime)
+            implementation(libs.ui.tooling.preview)
             implementation(libs.androidx.activity.compose)
 
             // Ktor
@@ -46,17 +46,17 @@ kotlin {
             implementation(libs.koin.android)
         }
         commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material3)
-            implementation(compose.ui)
-            implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
+            implementation(libs.runtime)
+            implementation(libs.foundation)
+            implementation(libs.material3)
+            implementation(libs.ui)
+            implementation(libs.components.resources)
+            implementation(libs.ui.tooling.preview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.androidx.navigation.compose)
-            implementation(compose.materialIconsExtended)
-            implementation(compose.material3AdaptiveNavigationSuite)
+            implementation(libs.material.icons.extended)
+            implementation(libs.material3.adaptive.navigation.suite)
 
             // Coil
             implementation(libs.coil.compose)
