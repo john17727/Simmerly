dd3f0bc021d64aa4742f9ae7bdbfd2e689e8074b
Juan Rincon <justanotherone17727@gmail.com>
2026-03-12 14:18:26 -0400

    add platform-specific dynamic color support files

---
+++ composeApp/src/jvmMain/kotlin/dev/juanrincon/simmerly/theme/DynamicColor.jvm.kt
@@ -0,0 +1,7 @@
+package dev.juanrincon.simmerly.theme
+
+import androidx.compose.material3.ColorScheme
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun rememberDynamicColorScheme(darkTheme: Boolean): ColorScheme? = null
