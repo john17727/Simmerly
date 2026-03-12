92eadb244d3de4be703e18c5cf779ddcb9f57815
Juan Rincon <justanotherone17727@gmail.com>
2026-03-12 10:03:27 -0400

    build: add stability analyzer plugin to all modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,4 +11,5 @@ plugins {
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.room) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.stability.analyzer) apply false
 }
