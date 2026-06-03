0500bf1ba94b607f2b5c0cec27e6c140f06b0e0b
Juan Rincon <justanotherone17727@gmail.com>
2026-06-03 17:07:43 -0400

    fix(data): simplify iOS NetworkHandler exception mapping Fixes compiler error where NSError check was always false Simplifies to return Unknown for unmapped platform exceptions

--- shared/src/iosMain/kotlin/dev/juanrincon/simmerly/core/data/remote/NetworkHandler.ios.kt
+++ shared/src/iosMain/kotlin/dev/juanrincon/simmerly/core/data/remote/NetworkHandler.ios.kt
@@ -7,18 +7,5 @@ import platform.Foundation.NSURLErrorDomain
 import platform.Foundation.NSURLErrorNetworkConnectionLost
 import platform.Foundation.NSURLErrorTimedOut
 
-actual fun <E> mapPlatformException(e: Exception): DataError.NetworkError<E> {
-    val cause = e.cause
-    if (cause is NSError) {
-        if (cause.domain == NSURLErrorDomain) {
-            return when (cause.code) {
-                NSURLErrorTimedOut -> DataError.NetworkError.RequestTimeout
-                NSURLErrorCannotConnectToHost,
-                NSURLErrorNetworkConnectionLost -> DataError.NetworkError.NoInternet
-                // Add more mappings as needed
-                else -> DataError.NetworkError.Unknown
-            }
-        }
-    }
-    return DataError.NetworkError.Unknown
-}
+actual fun <E> mapPlatformException(e: Exception): DataError.NetworkError<E> =
+    DataError.NetworkError.Unknown
